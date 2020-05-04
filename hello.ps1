878ee807dcd476fba79a29257c0b5bceb3b187cb
Koske Kimutai <koskedk@live.com>
2020-05-04 20:18:50 +0300

    unzip workflow

--- hello.ps1
+++ hello.ps1
@@ -1,6 +1,9 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $Url = 'https://github.com/koskedk/hello/releases/download/v1.0/hello.zip'
 $Destination= 'H:\DWAPI.Service_1_0_0_0\hello.zip'
+$DestinationFolder= 'H:\DWAPI.Service_1_0_0_0\hello\'
 
 $client = new-object System.Net.WebClient
 $client.DownloadFile($Url,$Destination)
+
+Expand-Archive -LiteralPath $Destination -DestinationPath DestinationFolder
