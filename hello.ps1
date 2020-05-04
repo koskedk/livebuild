623a660dc892cb265e3edc600047e917bbbd336a
Koske Kimutai <koskedk@live.com>
2020-05-04 20:14:52 +0300

    adding multiple workflows

--- hello.ps1
+++ hello.ps1
@@ -1,3 +1,4 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $Url = 'https://github.com/koskedk/hello/releases/download/v1.0/hello.zip'
 $Destination= 'H:\DWAPI.Service_1_0_0_0\hello.zip'
 
