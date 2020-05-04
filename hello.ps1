9b6598fc510e48cd062d6e3edab047f1ef2abda4
Koske Kimutai <koskedk@live.com>
2020-05-04 20:23:11 +0300

    unzip root

--- hello.ps1
+++ hello.ps1
@@ -1,7 +1,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $Url = 'https://github.com/koskedk/hello/releases/download/v1.0/hello.zip'
 $Destination= 'H:\DWAPI.Service_1_0_0_0\hello.zip'
-$DestinationFolder= 'H:\DWAPI.Service_1_0_0_0\hello'
+$DestinationFolder= 'H:\DWAPI.Service_1_0_0_0\'
 
 $client = new-object System.Net.WebClient
 $client.DownloadFile($Url,$Destination)
